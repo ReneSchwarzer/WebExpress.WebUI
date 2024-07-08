9b4c79615c41b9498dd1b10d31de087e2fe518b0
Rene Schwarzer <rene_schwarzer@hotmail.de>
2024-07-08 21:41:27 +0200

    add tests

---
+++ src/WebExpress.WebUI.Test/Control/UnitTestControlList.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using WebExpress.WebUI.WebControl;
+
+namespace WebExpress.WebUI.Test.Control
+{
+    /// <summary>
+    /// Tests the list control.
+    /// </summary>
+    public class UnitTestControlList
+    {
+        /// <summary>
+        /// Tests a empty list.
+        /// </summary>
+        [Fact]
+        public void EmptyList()
+        {
+            var context = new WebCore.WebPage.RenderContext();
+            var control = new ControlList();
+
+            Assert.Equal("<ul></ul>", control.Render(context).Trim());
+        }
+
+        /// <summary>
+        /// Tests a empty list.
+        /// </summary>
+        [Fact]
+        public void EmptyListGroup()
+        {
+            var context = new WebCore.WebPage.RenderContext();
+            var control = new ControlList() { Layout = TypeLayoutList.Group };
+
+            Assert.Equal("<ul class=\"list-group\"></ul>", control.Render(context).Trim());
+        }
+
+        /// <summary>
+        /// Tests a list.
+        /// </summary>
+        [Fact]
+        public void SimpleList()
+        {
+            var context = new WebCore.WebPage.RenderContext() { Culture = CultureInfo.CurrentCulture };
+            var item = new ControlListItem(new ControlText() { Text = "abc" });
+            var control = new ControlList(item);
+
+            Assert.Equal("<ul><li><div>abc</div></li></ul>", control.Render(context).Trim());
+        }
+    }
+}
---
+++ src/WebExpress.WebUI.Test/IHtmlNodeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using WebExpress.WebCore.WebHtml;
+
+namespace WebExpress.WebUI.Test
+{
+    internal static class IHtmlNodeExtensions
+    {
+        /// <summary>
+        /// Removes extra spaces and line breaks from the input node.
+        /// </summary>
+        /// <param name="node">The node to clean.</param>
+        /// <returns>A string with all consecutive spaces and line breaks reduced to single ones.</returns>
+        public static string Trim(this IHtmlNode node)
+        {
+            string withoutExtraSpaces = Regex.Replace(node.ToString().Trim(), @"\s+", " ");
+            string withoutExtraLineBreaks = Regex.Replace(withoutExtraSpaces, @"[\r\n]+", "\n");
+            string withoutSpacesBetweenBrackets = Regex.Replace(withoutExtraLineBreaks, @">\s+<", "><");
+
+            return withoutSpacesBetweenBrackets;
+        }
+    }
+}
